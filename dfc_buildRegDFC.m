67a7a3f2d9b2a38574b5b4cdac8cbfa1eb83dee6
Johnny Esquivel <jesquivel@futurex.com>
2021-06-09 18:46:14 -0500

    Save data file from GUI example

--- dfc_buildRegDFC.m
+++ dfc_buildRegDFC.m
@@ -1,86 +1,76 @@
-function dfc_buildRegDFC(regMap,subjProp,settings )
+function [RegDFCData, h] = dfc_buildRegDFC(regMap, subjProp)
 %DFC_BUILDRegDFC Build a .mat file with DFC Results
 %   Detailed explanation goes here
 %TODO: Add to action menu and generate after user is able to view region
 %average and can play with window length and stepsize to determine ideal
 %settings
-
+    bContinue = true;
     nRegions = length(regMap);
     nSubjects = length(subjProp);
     maxTP = subjProp(1).srcDim(4);
     
+    % If on windows, use parallel computing, on mac the OS does parallel itself
+    if strcmpi(computer('arch'),'win64')
+        usePfor = 1;
+    else
+        usePfor = 0;
+    end
+
     %TODO: Make window length and step size allow user inputs
     %User can configure after region averages generated and output to the
     %Dynaconn window
    
-    selection = input('use default Window Length and stepSize? Y/N ', 's')
+    selection = questdlg("Use default window length and step size?");
+    
     %Andrew and Zak added case for user input atlas and labels
     switch selection
-        case 'Y'
+        case 'Yes'
             windowLength = 32;
             stepSize = 8;  
 %             
-        case 'N'
+        case 'No'
             
-            prompt1 = 'Please provide the Window Length: ';
-            prompt2 = 'Please provide the Step Size: ';
-            windowLength = input(prompt1);
-            stepSize = input(prompt2);
+            prompt = {'Please provide the Window Length:', 'Please provide the Step Size:'};
+            dlgtitle = 'Window Step Selection';
+            dims = [1 35];
+            winstep = inputdlg(prompt, dlgtitle, dims);
+            windowLength = winstep{1};
+            stepSize = winstep{2};
            
-
+        case 'Cancel'
+            return
     end    
-    V = spm_vol(subjProp(s).srcFFile);    % open data file
-    nTP = length(V);
-    windowStart = [1:stepSize:nTP-windowLength+1];
-    numWindows=length(windowStart);
-    RegDFCData = zeros(nSubjects, nRegions, numRegions, numWindows);
+   
+    % Initialize h so that if the h message window is not needed it can be
+    % detected as not existing.
+    h = 0;
 
-    tic
-    % Build list of indices from each reg prob layer that is above 50
-    % The indices are without being masked
-    for i = 1 : length(regMap)
-        regMap(i).ind = find(regMap(i).prob > 50);
-    end
+    % Give user a message that we must build new data
+    set(0, 'DefaultUicontrolFontsize', 12);
+    Message = 'There isn''t any pre-existing data so new data is being built now';
+    Title = 'Corr. Running';
+    Icon = 'warn';
+    h = msgbox(Message,Title,Icon);
     
-    % Create progress bar for getting region average
-    title = 'Building Region Dynamic Functional Correlations';
-    hdl1 = 'Completion';
-    %hdl2 = 'Region Completion';
-    multiWaitbar( 'CloseAll', title );
-    if settings.usePfor
-        multiWaitbar( hdl1, title, 0, 'Color', 'g', 'CancelFcn', @(a,b) disp( ['Cancel ',a] ) );
-    else
-        multiWaitbar( hdl1, title, 0, 'Color', 'g', 'CanCancel', 'off');
-        multiWaitbar( hdl2, title, 0, 'Color', 'r', 'CancelFcn', @(a,b) disp( ['Cancel ',a] ) );
-    end
-    qString = 'Are you sure you want to cancel?';
-    abort = false;
+    % TODO(Johnny) not sure what this is so just make it fixed for now
+    numWindows = 5000;
     
+    RegDFCData = zeros(nSubjects, nRegions, numWindows);
     
+    tic;
+   
     
-  %This is the DFC Loop  
-    for iss=1:nSubjects,
-        iss=iss
-        
-        for iww = 1:numWindows
-            myavgsignal_window1 = squeeze(RegAveData(iss,:,(iww-1)*stepSize+1:(iww-1)*stepSize+windowLength)); %changed from myavgsignal to _allsubjects
-            myROIfc_window1 = corr(myavgsignal_window1');
-            RegDFCData(iss, :, :, iww)=myROIfc_window1;
+    % TODO(Johnny) Andrew, this is where you would do whatever analysis you are
+    % wanting to do.  For this example I'll just do an avg and you can
+    % replace it.
+    for ns = 1 : nSubjects
+        for nr = 1 : nRegions
+            for nw = 1 : numWindows
+                RegDFCData(ns, nr, nw) = ns*nr*nw;
+            end
         end
     end
-    
-        % Close the progress bar
-    multiWaitbar( hdl1, title, 'Close' );
-    multiWaitbar( hdl2, title, 'Close' );
 
     tEnd=toc;
     fprintf('Construction of region avgs took ');
     fprintf('%d minutes and %f seconds\n\n',floor(tEnd/60),rem(tEnd,60));
-    
-    if abort
-        errordlg( 'Didn''t get region means!');
-        return
-    else
-        % Save the data to mat file
-        save(settings.regAveFile,'RegAveData');
-    end
